3c1ac0b31c2e35b30b41370abbc7a133006736d9
Chris-Bortel <bortelcc19@gmail.com>
2021-01-03 13:10:55 -0800

    Rnadomizer is working

--- src/components/gameFront/gamePlay.js
+++ src/components/gameFront/gamePlay.js
@@ -5,6 +5,7 @@ import {useDispatch, useSelector} from 'react-redux';
 
 import * as actions from '../../store/game.js';
 
+import Prompts from './prompts.js'
 export default function GamePlay() {
     const count = useSelector( state => state.game.count )
     const prompts = useSelector( state => state.game.prompts )
@@ -13,20 +14,23 @@ export default function GamePlay() {
 
         
     function handleClick() {
-        dispatch(actions.randomize())
+        dispatch(actions.increment())
         console.log('clicked')
     }
 
     return (
     <View style={styles.container}>
+    <Text>
+        
+    <Prompts />
+    </Text>
+
     <Text h2>
         
       Count: {count}
         {/* <h2>{prompts}</h2> */}
     </Text>
-    <Text h2>
-        {prompts}
-    </Text>
+  
         <Button
         title="Bop It"
         onPress={() => handleClick()}
--- src/components/gameFront/prompts.js
+++ src/components/gameFront/prompts.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {useSelector} from 'react-redux';
-
-
+import {useDispatch, useSelector} from 'react-redux';
 export default function Prompts() {
-
+    const prompts = useSelector( state => state.game.prompts )
     return (
     <View style={styles.container}>
-    <Text>
-        <h1>Prompt Me</h1>
+    <Text h1>
+          {prompts[Math.floor(Math.random() * prompts.length)]}
     </Text>
     <StatusBar style="auto" />
     </View>
     );
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -24,3 +21,30 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import {useSelector} from 'react-redux';
+
+
+// export default function Prompts() {
+
+//     return (
+//     <View style={styles.container}>
+//     <Text>
+//         <h1>Prompt Me</h1>
+//     </Text>
+//     <StatusBar style="auto" />
+//     </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
